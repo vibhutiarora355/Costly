15aa8ad90fd1a93a6f0b0d2fa999143063b6dc9a
Alok <alokme123@gmail.com>
2021-12-10 17:18:58 +0530

    feat: backend stuff

--- backend/models/product.js
+++ backend/models/product.js
@@ -4,10 +4,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var product = new Schema(
 {
-    "productID": String,
-    "price": Number,
-    "name": String
+    id: String,
+    name: String,
+    price: Number,
+    description: String,
+    imageURL: String,
+    category: String
   });
 
 //Export model
-module.exports = mongoose.model('product', product);
+module.exports = mongoose.model('products', product);
--- backend/routes/addToCart.js
+++ backend/routes/addToCart.js
@@ -6,76 +6,107 @@ var ProductsModel = require('../models/product.js');
 var CartsModel = require('../models/cart.js');
 
 function connectToDB() {
-    //Set up mongoose connection
-    var mongoDB = process.env.DB_URL;
-    mongoose.connect(mongoDB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-  var db = mongoose.connection;
-  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  }
+	//Set up mongoose connection
+	var mongoDB = process.env.DB_URL;
+	mongoose.connect(mongoDB, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	});
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+}
 
 connectToDB();
 const router = express.Router()
 
-router.get("/getCart", (req,res) => {
-    var user = req.query.user;
-    CartsModel
-  .find({
-    user: user
-  })
-  .exec((err, entry) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).send(err)
-    }
-    console.log(entry)
-    return res.status(200).send(entry[0]);
-  }); 
+router.get("/getCart", (req, res) => {
+	var user = req.query.user;
+	CartsModel
+		.findOne({
+			user: user
+		})
+		.exec((err, entry) => {
+			if (err) {
+				console.log(err);
+				return res.status(500).send(err)
+			}
+			console.log(entry)
+			return res.status(200).send(entry);
+		});
 })
 
-router.get("/addToCart", async (req,res) => {
-    let user = req.query.user;
-    let ID = req.query.productID;
-    let price = req.query.price;
-    let name = req.query.name;
-    // "user":user,
-    // let product = new ProductsModel({
-    //     "productID": ID,
-    //     "price": price,
-    //     "name": name
-    // });
+async function cartAdd(itemToAdd,user, res) {
+	// let currentCart;
+	console.log(user);
+	let currentCart = await CartsModel
+		.findOne({
+			user: user
+		})
+	console.log(`Current cart before update: ${currentCart}`);
+	if (currentCart != null) {
+		console.log("Current cart not empty!")
+		let itemFound = false;
+		currentCart.items.forEach((item) => {
+			console.log(`${itemToAdd.productID} ${item.productID}`)
+			if (itemToAdd.productID === item.productID) {
+				itemFound = true;
+				item.quantity += 1;
+			}
+		});
+		if (!itemFound) {
+			console.log("Item not found in Cart");
+			currentCart.items.push(itemToAdd);
+		}
+	} else {
+		currentCart = new CartsModel({
+			user: user,
+			items: [
+				itemToAdd
+			]
+		})
+	}
+	let newCurrentCart = {
+		user: currentCart.user,
+		items: currentCart.items
+	}
+	console.log(`Current cart after update: ${newCurrentCart.items}`);
+	await CartsModel.updateOne({ user: user }, newCurrentCart, { upsert: true }, () => {
+		console.log("Write Success");
+		return res.status(200).send({ "status": "success" });
+	}) 
+	// return res.status(401).send({ "status": "failed" });
+}
 
-    let currentCart = new CartsModel({
-        user:user,
-        items:[
-            {
-                productID:ID,
-                price:price,
-                name:name,
-                quantity: 1
-            }
-        ],
-    });
-    console.log(currentCart);
-    // await CartsModel
-    //     .find({
-    //         user: user
-    //     })
-    //     .exec((err, entry) => {
-    //         if (err) {
-    //         console.log(err);
-    //         }
-    //         currentCart = entry;
-    //     });
-    // console.log(currentCart);
-    // currentCart.items.append(product);
-  await currentCart.save();
-  return res.status(200).send({"status":"success"});
-    //ProductsModel.updateOne({gatewayID: gatewayID,nodeID:nodeID}, data, {upsert: true},()=>{
-    //   console.log("Write success");
-    // });
+
+router.get("/secureAddToCart", async (req, res) => {
+	let user = req.query.user;
+	let ID = req.query.productID;
+	let product = await ProductsModel.findOne({ id: ID })
+	if(product != null) {
+		let itemToAdd = {
+			productID: ID,
+			price: product.price,
+			name: product.name,
+			quantity: 1,
+			imageURL: product.imageURL
+		};
+		await cartAdd(itemToAdd, user, res);
+	} else {
+		return res.status(401).send({"status":"product not found"})
+	}
+});
+
+router.get("/addToCart", async (req, res) => {
+	let user = req.query.user;
+	let itemToAdd = {
+		productID: req.query.productID,
+		price: req.query.price,
+		name: req.query.name,
+		quantity: 1,
+		imageURL: req.query.imageURL
+	};
+	console.log(`Item to be added: ${itemToAdd.name}`);
+	await cartAdd(itemToAdd, user, res);
 })
 
 module.exports = router
